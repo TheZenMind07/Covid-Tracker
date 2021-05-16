b181f1e5e556dbad80a04112f02272ed56f7a471
Rajkishor Pradhan <rajp7598@gmail.com>
2021-05-16 12:14:06 +0530

    Updates

---
+++ precache-manifest.436ac230a812b981d26cec32f38d149d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2926a1e121a07f41f295c3dd69d8d9ac",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "09862ba2dddfe976adbe",
+    "url": "/covid-19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "7c2eee6591492ba022fb",
+    "url": "/covid-19-tracker/static/css/main.575429ae.chunk.css"
+  },
+  {
+    "revision": "09862ba2dddfe976adbe",
+    "url": "/covid-19-tracker/static/js/2.c35eea8b.chunk.js"
+  },
+  {
+    "revision": "88ab8e9bfa1a02a6d25f3aa28a50aeac",
+    "url": "/covid-19-tracker/static/js/2.c35eea8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c2eee6591492ba022fb",
+    "url": "/covid-19-tracker/static/js/main.c0487af1.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
